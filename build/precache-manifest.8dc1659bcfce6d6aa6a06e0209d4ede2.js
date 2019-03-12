ed3b304d9daf9c89eed72be427c27f44a5976730
Max Kozyritsky <maxim.kozyricki@gmail.com>
2019-03-12 12:32:33 +0300

    init: initial commit

---
+++ build/precache-manifest.8dc1659bcfce6d6aa6a06e0209d4ede2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9e3bfa7241d50db2c887",
+    "url": "/static/js/main.9e3bfa72.chunk.js"
+  },
+  {
+    "revision": "aa0fdcee49e6c18947c5",
+    "url": "/static/js/2.aa0fdcee.chunk.js"
+  },
+  {
+    "revision": "ad31da15ecfe8f33f06f415d881a85f7",
+    "url": "/index.html"
+  }
+];
